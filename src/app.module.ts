27d07b87a85335b68c70bff13be5ccb8f66a1a51
DanSylvest <cublakhan257@gmail.com>
2025-06-02 08:45:00 +0300

    add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RouteModule } from './route/route.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [RouteModule],
+  imports: [RouteModule, HealthModule],
   controllers: [],
   providers: [],
 })
